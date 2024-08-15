3a6e3cfe6f203d2eec7bba877fc3e10a47f5557e
mpan <mp322@st-andrews.ac.uk>
2024-08-15 12:46:14 +0100

    finished basic impl mostly - now testing (should have done this sooner lol)

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,17 +1,73 @@
-use core::panicking::panic;
-use std::{io, usize};
+use std::usize;
 
 use crate::tokens::Token;
 
 pub struct Interpreter {
+    program: Vec<Token>,
     memory: Vec<u8>,
     dp: usize,
     ip: usize,
 }
 
 impl Interpreter {
-    pub fn new(size: usize) -> Interpreter {
+    fn read_memory(&self) -> u8 {
+        return self.memory[self.dp];
+    }
+    fn add_memory(&mut self, n: u32) {
+        self.memory[self.dp] += n as u8;
+    }
+    fn sub_memory(&mut self, n: u32) {
+        self.memory[self.dp] -= n as u8;
+    }
+    fn branch_left(&mut self) {
+        // if the memory is not zero, skip the bracket
+        if self.read_memory() != 0 {
+            self.ip += 1;
+            return;
+        }
+
+        // go to matching closing bracket
+        let mut depth = 1;
+        while depth > 0 {
+            let token = self.program.get(self.ip);
+            match token {
+                Some(v) => match v {
+                    Token::LBrack => depth += 1,
+                    Token::RBrack => depth -= 1,
+                    _ => {}
+                },
+                None => panic!("No matching closing bracket found!"),
+            }
+            self.ip += 1;
+        }
+    }
+
+    fn branch_right(&mut self) {
+        // if the memory is not zero, skip the bracket
+        if self.read_memory() != 0 {
+            self.ip += 1;
+            return;
+        }
+
+        // go to matching opening bracket
+        let mut depth = 1;
+        while depth > 0 {
+            let token = self.program.get(self.ip);
+            match token {
+                Some(v) => match v {
+                    Token::LBrack => depth -= 1,
+                    Token::RBrack => depth += 1,
+                    _ => {}
+                },
+                None => panic!("No matching closing bracket found!"),
+            }
+            self.ip -= 1;
+        }
+    }
+
+    pub fn new(size: usize, program: Vec<Token>) -> Interpreter {
         return Interpreter {
+            program,
             memory: vec![0; size],
             ip: 0,
             dp: 0,
@@ -28,24 +84,13 @@ impl Interpreter {
 
             match token {
                 Token::Input => todo!(),
-                Token::Output => print!("{}", self.memory[self.dp]),
-                Token::Inc(n) => self.memory[self.dp] += 1,
-                Token::Dec(n) => self.memory[self.dp] -= 1,
-                Token::LBrack(n) => self.ip = *n,
-                Token::RBrack(n) => self.ip = *n,
-                Token::IncDP(n) => {
-                    if self.dp < *n { // TODO: maybe remove this check and allow for later failure
-                        panic!("Attempted to move data pointer out of bounds")
-                    }
-                    self.dp -= n
-                }
-                Token::DecDP(n) => {
-                    let max = self.memory.len() - *n - 1;
-                    if self.dp > max { // TODO: maybe remove this check and allow for later failure
-                        panic!("Attempted to move data pointer out of bounds")
-                    }
-                    self.dp -= n
-                }
+                Token::Output => print!("{}", self.read_memory()),
+                Token::Inc(n) => self.add_memory(*n),
+                Token::Dec(n) => self.sub_memory(*n),
+                Token::LBrack => self.branch_left(),
+                Token::RBrack => self.branch_right(),
+                Token::IncDP(n) => self.dp -= n,
+                Token::DecDP(n) => self.dp -= n,
             }
         }
     }
--- src/tokens.rs
+++ src/tokens.rs
@@ -1,6 +1,7 @@
 use core::panic;
 use std::usize;
 
+#[derive(Debug, PartialEq)]
 pub enum Token {
     Inc(u32),
     Dec(u32),
@@ -26,28 +27,61 @@ fn is_repeatable(char: u8) -> bool {
         _ => false,
     };
 }
+fn is_valid_char(char: u8) -> bool {
+    return match char {
+        b'[' | b']' | b',' | b'.' | b'+' | b'-' | b'>' | b'<' => true,
+        _ => false,
+    };
+}
 
 impl Tokenizer {
+    pub fn new(input: Vec<u8>) -> Tokenizer {
+        let next = input.get(0);
+        let next = match next {
+            None => 0,
+            Some(v) => *v,
+        };
+        return Tokenizer {
+            input,
+            idx: 0,
+            curr: 0,
+            next,
+            done: false,
+        };
+    }
+
     fn is_done(&self) -> bool {
         return self.done;
     }
 
-    fn next_char(&self) -> bool {
-        let next_char = self.input.get(self.idx);
-        match next_char {
-            Some(v) => {
-                self.curr = self.next;
-                self.next = *v;
-                self.idx += 1;
-            }
-            None => {
-                self.next = 0;
-                self.curr = self.next;
-                if self.curr == 0 {
-                    self.done = true
+    fn next_char(&mut self) -> bool {
+        loop {
+            let next_char = self.input.get(self.idx + 1);
+            match next_char {
+                Some(v) => {
+                    // skip over invalid characters
+                    if !is_valid_char(*v) {
+                        continue;
+                    }
+                    self.curr = self.next;
+                    self.next = *v;
+                    self.idx += 1;
+                }
+                None => {
+                    self.curr = self.next;
+                    self.next = 0;
+                    if self.curr == 0 {
+                        self.done = true
+                    }
                 }
             }
+            // once a valid character is found, or the end of the input is reached, break out of the loop
+            break;
         }
+        println!(
+            "curr: {}, next: {}, done: {}",
+            self.curr as char, self.next as char, self.done
+        );
         return self.done;
     }
 }
@@ -65,7 +99,7 @@ impl Iterator for Tokenizer {
 
         // condense sequences of characters which are repeatable into 1 token
         if is_repeatable(char) {
-            let mut count: u32 = 0;
+            let mut count: u32 = 1;
             while self.curr == self.next && !self.is_done() {
                 self.next_char();
                 count += 1;
@@ -86,8 +120,91 @@ impl Iterator for Tokenizer {
             b']' => Token::RBrack,
             b',' => Token::Input,
             b'.' => Token::Output,
-            // ignore all other tokens
+            _ => panic!("Unexpected character {} when tokenizing", char),
         };
         return Some(token);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn string_to_vec(s: &str) -> Vec<u8> {
+        let mut v = Vec::new();
+        s.chars().for_each(|c| v.push(c as u8));
+        return v;
+    }
+
+    #[test]
+    fn test_basic() {
+        let mut tokenizer = Tokenizer::new(string_to_vec(".-+><[],"));
+        let expected = vec![
+            Token::Output,
+            Token::Dec(1),
+            Token::Inc(1),
+            Token::IncDP(1),
+            Token::DecDP(1),
+            Token::LBrack,
+            Token::RBrack,
+            Token::Input,
+        ];
+
+        for result in expected.iter() {
+            let token = tokenizer.next();
+            if let Some(token) = token {
+                assert_eq!(token, *result);
+            } else {
+                assert_ne!(token, None);
+            }
+        }
+    }
+
+    #[test]
+    fn test_repeatable() {
+        let mut tokenizer = Tokenizer::new(string_to_vec("+-->>><<<<"));
+        let expected = vec![
+            Token::Inc(1),
+            Token::Dec(2),
+            Token::IncDP(3),
+            Token::DecDP(4),
+        ];
+
+        for result in expected.iter() {
+            let token = tokenizer.next();
+            if let Some(token) = token {
+                assert_eq!(token, *result);
+            } else {
+                assert_ne!(token, None);
+            }
+        }
+    }
+
+    #[test]
+    fn test_repeatable_start() {
+        let mut tokenizer = Tokenizer::new(string_to_vec("++++"));
+        let expected = vec![
+            Token::Inc(4),
+        ];
+
+        for result in expected.iter() {
+            let token = tokenizer.next();
+            if let Some(token) = token {
+                assert_eq!(token, *result);
+            } else {
+                assert_ne!(token, None);
+            }
+        }
+    }
+
+    #[test]
+    fn test_end_input() {
+        let mut tokenizer = Tokenizer::new(string_to_vec("++++."));
+        tokenizer.next();
+        tokenizer.next();
+        let token = tokenizer.next();
+        assert_eq!(token, None);
+        let token = tokenizer.next();
+        assert_eq!(token, None);
     }
 }
